3c93c54b587847c62927153988814b62d5d85f9f
Foxxo Dev <gammert54@gmail.com>
2024-02-22 20:40:51 +0100

    Initial Commit

---
+++ js/home-redirects.js
@@ -0,0 +1,9 @@
+const chat = document.getElementById('chat');
+const startBtn = document.getElementById('strt-nw');
+
+startBtn.addEventListener('click', () => {
+  window.location.href = '/work-in-progress/index.html';
+});
+chat.addEventListener('click', () => {
+  window.location.href = '/clearnet-unsupported/index.html';
+});
