5e5883ffa4b5bf880aeb7592a0e25c958b74a991
Leon <leon.potocki@gmail.com>
2024-03-24 18:58:45 +0000

    big changes

--- js/home-redirects.js
+++
@@ -1,9 +0,0 @@
-const chat = document.getElementById('chat');
-const startBtn = document.getElementById('strt-nw');
-
-startBtn.addEventListener('click', () => {
-  window.location.href = '/work-in-progress/index.html';
-});
-chat.addEventListener('click', () => {
-  window.location.href = '/clearnet-unsupported/index.html';
-});
